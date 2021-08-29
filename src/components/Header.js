f794f74cbbcd0a1de46ba7838cd8e27c7fe47542
hakandrsn <hakandrsnn@gmail.com>
2021-08-29 12:51:07 +0300

    header components created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,23 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import {Text, SafeAreaView, TouchableOpacity } from 'react-native'
+import { styles } from '../GlobalStyles'
+
 
 const Header = () => {
     return (
-        <View>
-            <Text></Text>
-        </View>
+        <SafeAreaView style={styles.HeaderBar}>
+            <TouchableOpacity style={styles.HeaderBarCancel}>
+                <Text style={styles.HeaderTxtSide}>Cancel</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Text style={styles.HeaderTxt}>Choose Music</Text>
+
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.HeaderBarDone}>
+                <Text style={styles.HeaderTxtSide}>Done</Text>
+
+            </TouchableOpacity>
+        </SafeAreaView>
     )
 }
 
