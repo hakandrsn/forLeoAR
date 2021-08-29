80013ac4fa2f3bb9d0e8bbf75ce3f171faee391f
hakandrsn <hakandrsnn@gmail.com>
2021-08-29 12:52:21 +0300

    Component of header styles created

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -9,5 +9,33 @@ export const styles = StyleSheet.create({
         flex: 1,
 
     },
+    //Header Styles
+
+    HeaderBar: {
+        flexDirection: "row",
+        width: widthDim,
+        flex: .12,
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        borderBottomWidth: .2
+    },
+    HeaderBarCancel: {
+        marginRight: 45
+    },
+    HeaderBarChoose: {
+
+    },
+    HeaderBarDone: {
+        marginLeft: 45
+    },
+    HeaderTxt: {
+        fontSize: 17,
+        fontWeight: "bold",
+
+    },
+    HeaderTxtSide: {
+        fontSize: 14,
+        fontWeight: "400"
+    },
 
 })
